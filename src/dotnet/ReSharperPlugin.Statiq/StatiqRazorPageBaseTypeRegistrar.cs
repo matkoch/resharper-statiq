6a490ca4468eae7cae57af052114ce59d20a0a8d
Matthias Koch <ithrowexceptions@gmail.com>
2021-08-24 23:37:18 +0200

    Initial commit

---
+++ src/dotnet/ReSharperPlugin.Statiq/StatiqRazorPageBaseTypeRegistrar.cs
@@ -0,0 +1,31 @@
+using JetBrains.Application;
+using JetBrains.Application.Settings;
+using JetBrains.Application.Settings.Implementation;
+using JetBrains.Metadata.Utils;
+using JetBrains.ReSharper.Psi.Razor.Settings;
+using JetBrains.Util;
+
+namespace ReSharperPlugin.Statiq
+{
+    [ShellComponent]
+    public class StatiqRazorBaseTypesSettings : HaveDefaultSettings
+    {
+        public StatiqRazorBaseTypesSettings(ILogger logger, ISettingsSchema settingsSchema)
+            : base(settingsSchema, logger)
+        {
+        }
+
+        public override string Name => "Statiq Razor Base Types Settings";
+
+        public override void InitDefaultSettings(ISettingsStorageMountPoint mountPoint)
+        {
+            SetIndexedValue(mountPoint,
+                RazorBaseTypesSettingsAccessor.Assemblies,
+                AssemblyNameInfo.Parse("Statiq.Razor"),
+                new RazorBaseTypesSettings.BaseTypeInfo
+                {
+                    { "Statiq.Razor.StatiqRazorPage", default }
+                });
+        }
+    }
+}
