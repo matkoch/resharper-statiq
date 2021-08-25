9b217c0ecbb3207ce53a23d5c5a0b3b8a3ba7bd7
Matthias Koch <ithrowexceptions@gmail.com>
2021-08-25 14:57:28 +0200

    Disable annotations

--- src/dotnet/ReSharperPlugin.Statiq/StatiqRazorPageBaseTypeRegistrar.cs
+++ src/dotnet/ReSharperPlugin.Statiq/StatiqRazorPageBaseTypeRegistrar.cs
@@ -7,25 +7,25 @@ using JetBrains.Util;
 
 namespace ReSharperPlugin.Statiq
 {
-    [ShellComponent]
-    public class StatiqRazorBaseTypesSettings : HaveDefaultSettings
-    {
-        public StatiqRazorBaseTypesSettings(ILogger logger, ISettingsSchema settingsSchema)
-            : base(settingsSchema, logger)
-        {
-        }
-
-        public override string Name => "Statiq Razor Base Types Settings";
-
-        public override void InitDefaultSettings(ISettingsStorageMountPoint mountPoint)
-        {
-            SetIndexedValue(mountPoint,
-                RazorBaseTypesSettingsAccessor.Assemblies,
-                AssemblyNameInfo.Parse("Statiq.Razor"),
-                new RazorBaseTypesSettings.BaseTypeInfo
-                {
-                    { "Statiq.Razor.StatiqRazorPage", default }
-                });
-        }
-    }
+    // [ShellComponent]
+    // public class StatiqRazorBaseTypesSettings : HaveDefaultSettings
+    // {
+    //     public StatiqRazorBaseTypesSettings(ILogger logger, ISettingsSchema settingsSchema)
+    //         : base(settingsSchema, logger)
+    //     {
+    //     }
+    //
+    //     public override string Name => "Statiq Razor Base Types Settings";
+    //
+    //     public override void InitDefaultSettings(ISettingsStorageMountPoint mountPoint)
+    //     {
+    //         SetIndexedValue(mountPoint,
+    //             RazorBaseTypesSettingsAccessor.Assemblies,
+    //             AssemblyNameInfo.Parse("Statiq.Razor"),
+    //             new RazorBaseTypesSettings.BaseTypeInfo
+    //             {
+    //                 { "Statiq.Razor.StatiqRazorPage", default }
+    //             });
+    //     }
+    // }
 }
